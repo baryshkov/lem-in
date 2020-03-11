841a0de82a69b68df4f39e8d89790892521beceb
baryshkov <ambaryshkov@gmail.com>
2020-03-11 22:13:53 +0300

    test api

--- src/server.js
+++ src/server.js
@@ -12,4 +12,5 @@ app.get('/ping', (req, res) => res.send('pong'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 app.get('/struct', (req, res) => res.send(struct));
 console.log('server is running');
+console.log(struct);
 app.listen( port, () => console.log(`Listening on ${port}`));
